fd15cbc54bc5bfd6b30b62aa6c25248aebc2b7fb
Amey-274 <nonhackable1290@gmail.com>
2021-01-24 08:42:09 +0530

    add func3 form mumbai

--- file.py
+++ file.py
@@ -1,10 +1,13 @@
 def func1():
-<<<<<<< HEAD
-    return "hello world"
-=======
     return "hello world"
 
 
 def func2():
     return "Hello world"
->>>>>>> 8323185b09f4ce2fcad813d07d04519d4f26dbf2
+
+
+def func3():
+    return "hello World"
+
+
+    
